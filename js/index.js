a68ae6e2fbe17e3f74d52b1d7d227fd43e3c30d7
Ben Miller <bmiller@plos.org>
2019-01-04 12:20:51 -0800

    Use jsonp

--- js/index.js
+++ js/index.js
@@ -65,7 +65,7 @@ function refreshNames () {
 
 // leaderboard
 
-var search = "https://journals.plos.org/plosone/dynamicSearch?resultsPerPage=5&sortOrder=MOST_VIEWS_ALL_TIME&q=publication_date:"
+var search = "https://api.plos.org/search?callback=?"
 // publication_date:[2018-10-01T00:00:00 TO 2018-12-21T23:59:59Z]
 $(document).ready(function(){
   var now = new Date
@@ -76,28 +76,31 @@ $(document).ready(function(){
   var lastMonth = new Date(nowMs - (30 * oneDayMs))
   var lastYear = new Date(nowMs - (365 * oneDayMs))
 
-  $.getJSON(search + `[${lastWeek.toISOString()} TO ${now.toISOString()}]`, function(data) {
-    console.log(data)
-    $('<ul/>', {class: "list-group", html: buildListItems(data.searchResults.docs)}).appendTo('#week')
-  })
-
-  $.getJSON(search + `[${lastMonth.toISOString()} TO ${now.toISOString()}]`, function(data) {
-    console.log(data)
-    $('<ul/>', {class: "list-group", html: buildListItems(data.searchResults.docs)}).appendTo('#month')
-  })
-
-  $.getJSON(search + `[${lastYear.toISOString()} TO ${now.toISOString()}]`, function(data) {
-    console.log(data)
-    $('<ul/>', {class: "list-group", html: buildListItems(data.searchResults.docs)}).appendTo('#year')
-  })
+  $.getJSON(search, queryStringData(lastWeek, now, 'lastWeekCallback'))
+  $.getJSON(search, queryStringData(lastMonth, now, 'lastMonthCallback'))
+  $.getJSON(search, queryStringData(lastYear, now, 'lastYearCallback'))
 })
 
+function lastWeekCallback(data) { buildListItems(data, '#week') }
+function lastMonthCallback(data) { buildListItems(data, '#month') }
+function lastYearCallback(data) { buildListItems(data, '#year') }
 
-function buildListItems (docs) {
-  return docs.map(function(article){
+function queryStringData(startDate, endDate, callback, counter='counter_total_all') {
+  return {
+    sort: `${counter} desc`,
+    fl: `id,title,journal_name,${counter}`,
+    wt: 'json',
+    'json.wrf': callback,
+    q: `publication_date:[${startDate.toISOString()} TO ${endDate.toISOString()}]`
+  }
+}
+
+function buildListItems (data, elementId) {
+  var listItems =  data.response.docs.map(function(article){
     var journal = `<h6>${article.journal_name}</h6>`
     var anchor = `<p><a href="https://journals.plos.org${article.link}">${article.title}</a></p>`
     var badge = `<span class="badge">${article.counter_total_all}</span>`
     return `<li class="list-group-item">${badge}${journal}${anchor}</li>`
   }).join('')
+  $('<ul/>', {class: "list-group", html: listItems}).appendTo(elementId)
 }
